c9ec2181d4d8a07cc899c6e5c07226d21a8140a6
Soham Salkar <salkarsoham2@gmail.com>
2021-04-23 12:38:34 +0530

    Initial commit

---
+++ src/main/java/com/soham/springbootWebSocketWebChat/SpringBootWebSocketWebChatApplication.java
@@ -0,0 +1,13 @@
+package com.soham.springbootWebSocketWebChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketWebChatApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketWebChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soham/springbootWebSocketWebChat/SpringBootWebSocketWebChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soham.springbootWebSocketWebChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSocketWebChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
